715d5795b0d26c31dedbfd79fd3d0ee6cb1c5cc1
smilevent <1028330144@qq.com>
2021-07-13 18:57:22 +0800

    add my favorite tieba info

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const crypto = require('crypto');
 const axios = require('axios')
+const cheerio = require('cheerio');
 
 async function get_tbs(BDUSS) {
     let url = 'http://tieba.baidu.com/dc/common/tbs'
@@ -39,6 +40,24 @@ async function sign(BDUSS, kw) {
     let response = await axios.post(url, _data, { headers })
     return response['data']
 
+}
+
+async function 获取我关注的贴吧(BDUSS) {
+    const url = 'http://tieba.baidu.com/mo/q---B8D06B9EB00241F919F47789D4FD3103%3AFG%3D1--1-1-0--2--wapp_1385540291997_626/m?tn=bdFBW&tab=favorite'
+    let headers = { 'Cookie': `BDUSS=${BDUSS}` }
+    let res = await axios.get(url, { headers })
+    let html = res['data']
+    const $ = cheerio.load(html)
+    let tr = $('table tr')
+    let result = []
+    tr.each(function (i, el) {
+        let id = $(el).children('td:nth-child(1)').text().split('.')[0]
+        let kw = $(el).children('td:nth-child(1)').text().split('.')[1]
+        let level = $(el).children('td:nth-child(2)').text().split('等级')[1]
+        let exp = $(el).children('td:nth-child(3)').text().split('经验值')[1]
+        result[i] = { id, kw, level, exp }
+    })
+    return result
 }
 
 exports.tieba_sign = sign;
